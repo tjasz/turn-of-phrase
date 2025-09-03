e4d7658062defcbf7e84098f0236316fb4428a73
tjasz <nukorausa@hotmail.com>
2025-09-03 13:28:43 -0700

    Timer improvements

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -6,7 +6,9 @@ interface TimerProps {
 
 const Timer: React.FC<TimerProps> = ({ timeLeft }) => (
   <div className="timer">
-    <span>Time Left: <pre className="counter">{timeLeft.toString().padStart(2, ' ')}</pre>s</span>
+    <span style={{ color: timeLeft === 0 ? 'red' : undefined }}>
+      Time Left: <span className="counter">{timeLeft.toString().padStart(2, ' ')}</span>s
+    </span>
   </div>
 );
 
