27e3d1c80378bf4f5e4cc39dc8076aa8306ebc30
tjasz <nukorausa@hotmail.com>
2025-09-04 09:12:30 -0700

    Style the timer more neutral

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -10,7 +10,7 @@ const Timer: React.FC<TimerProps> = ({ timeLeft, totalTime }) => {
   const percentLeft = (timeLeft / totalTime) * 100;
   return (
     <div className="timer" style={{
-      background: `linear-gradient(to right, #8c8 ${percentLeft}%, #c88 ${percentLeft}% 100%)`
+      background: `linear-gradient(to right, #888 ${percentLeft}%, transparent ${percentLeft}% 100%)`
     }}>
       <span>
         Time Left: <Counter value={Math.ceil(timeLeft / 1000)} width={2} />s
