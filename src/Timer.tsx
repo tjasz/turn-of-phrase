0ead37e787258d959895d66bbd5ed05ae94a10d5
tjasz <nukorausa@hotmail.com>
2025-09-05 09:25:26 -0700

    Use color variables for consistent theme

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -10,7 +10,7 @@ const Timer: React.FC<TimerProps> = ({ timeLeft, totalTime }) => {
   const percentLeft = (timeLeft / totalTime) * 100;
   return (
     <div className="timer" style={{
-      background: `linear-gradient(to right, #888 ${percentLeft}%, transparent ${percentLeft}% 100%)`
+      background: `linear-gradient(to right, var(--accent) ${percentLeft}%, transparent ${percentLeft}% 100%)`
     }}>
       <span>
         Time Left: <Counter value={Math.ceil(timeLeft / 1000)} width={2} />s
