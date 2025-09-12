3d3b2e11c297d46dd9ba1c2dce55d18ea0d98239
tjasz <nukorausa@hotmail.com>
2025-09-12 08:26:28 -0700

    Change color of timer text at <50%

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -7,14 +7,14 @@ interface TimerProps {
 }
 
 const Timer: React.FC<TimerProps> = ({ timeLeft, totalTime }) => {
-  const percentLeft = (timeLeft / totalTime) * 100;
+  const percentLeft = Math.floor((timeLeft / totalTime) * 100);
   return (
     <div className="timer" style={{
-      background: `linear-gradient(to right, var(--accent) ${percentLeft}%, transparent ${percentLeft}% 100%)`
+      background: `linear-gradient(to right, var(--accent) ${percentLeft}%, transparent ${percentLeft}% 100%)`,
+      color: percentLeft < 50 ? 'var(--text)' : 'var(--accent-text)',
+      textShadow: `${percentLeft < 50 ? '#0000' : 'var(--shadowColor)'} 0 0 4px`,
     }}>
-      <span>
-        Time Left: <Counter value={Math.ceil(timeLeft / 1000)} width={2} />s
-      </span>
+      <span>Time Left: <Counter value={Math.ceil(timeLeft / 1000)} width={2} />s</span>
     </div>
   );
 }
