bf10209fa1c3cf597a8438ae3226684c9b466a54
tjasz <nukorausa@hotmail.com>
2025-09-16 21:53:41 -0700

    Flesh out MUI theme colors

--- src/muiTheme.ts
+++ src/muiTheme.ts
@@ -11,6 +11,36 @@ export const muiTheme = createTheme({
           dark: '#04a',
           contrastText: '#def'
         },
+        secondary: {
+          main: '#86c',
+          light: '#a8e',
+          dark: '#64a',
+          contrastText: '#def'
+        },
+        error: {
+          main: '#800',
+          light: '#a22',
+          dark: '#600',
+          contrastText: '#123',
+        },
+        warning: {
+          main: '#660',
+          light: '#882',
+          dark: '#440',
+          contrastText: '#123',
+        },
+        info: {
+          main: '#26c',
+          light: '#48e',
+          dark: '#04a',
+          contrastText: '#def'
+        },
+        success: {
+          main: '#080',
+          light: '#2a2',
+          dark: '#060',
+          contrastText: '#123'
+        },
       },
     },
     dark: {
@@ -20,6 +50,36 @@ export const muiTheme = createTheme({
           light: '#48e',
           dark: '#04a',
           contrastText: '#def'
+        },
+        secondary: {
+          main: '#86c',
+          light: '#a8e',
+          dark: '#64a',
+          contrastText: '#def'
+        },
+        error: {
+          main: '#c44',
+          light: '#e66',
+          dark: '#a22',
+          contrastText: '#123',
+        },
+        warning: {
+          main: '#cc4',
+          light: '#ee6',
+          dark: '#aa2',
+          contrastText: '#123',
+        },
+        info: {
+          main: '#26c',
+          light: '#48e',
+          dark: '#04a',
+          contrastText: '#def'
+        },
+        success: {
+          main: '#4c4',
+          light: '#6e6',
+          dark: '#2a2',
+          contrastText: '#123'
         },
       },
     },
