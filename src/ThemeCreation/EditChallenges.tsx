f12b6605827a03b989df480bd034b4c5c0e75662
tjasz <nukorausa@hotmail.com>
2025-09-25 11:45:10 -0700

    Show challenges in accordion

--- src/ThemeCreation/EditChallenges.tsx
+++ src/ThemeCreation/EditChallenges.tsx
@@ -1,5 +1,5 @@
-import { Box, IconButton, TextField } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, IconButton, TextField } from "@mui/material";
+import { Delete, ExpandMore } from "@mui/icons-material";
 
 interface IEditChallengesProps {
   challenges: Challenge[];
@@ -11,27 +11,37 @@ const EditChallenges: React.FC<IEditChallengesProps> = ({ challenges, setChallen
   return (
     <Box>
       {challenges.map((challenge, i) => (
-        <Box key={i}>
-          <TextField
-            label={`Challenge ${i + 1}`}
-            value={JSON.stringify(challenge)}
-            onChange={(e) => setChallenges(challenges.map((ch, idx) => idx === i ? JSON.parse(e.target.value) : ch))}
-            fullWidth
-            variant="outlined"
-            margin="normal"
-            slotProps={{
-              input: {
-                endAdornment: (
-                  <>
-                    <IconButton title="Delete" onClick={() => setChallenges(challenges.filter((_, idx) => i !== idx))}>
-                      <Delete />
-                    </IconButton>
-                  </>
-                )
-              }
-            }}
-          />
-        </Box>
+        <Accordion key={i}>
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            {challenge.Main
+              ? `${challenge.Main}${challenge.Related.filter(r => r.trim() !== "").length ? ` - (${challenge.Related.filter(r => r.trim() !== "").join(", ")})` : ""}`
+              : "New Challenge"}
+          </AccordionSummary>
+          <AccordionDetails>
+            <TextField
+              label="Main Challenge"
+              value={challenge.Main}
+              onChange={(e) => setChallenges(challenges.map((ch, idx) => idx === i ? { ...ch, Main: e.target.value } : ch))}
+              fullWidth
+              variant="outlined"
+              margin="normal"
+            />
+            {challenge.Related.map((related, j) => (
+              <TextField
+                key={j}
+                label={`Related Phrase ${j + 1}`}
+                value={related}
+                onChange={(e) => setChallenges(challenges.map((ch, idx) => idx === i ? { ...ch, Related: ch.Related.map((r, rIdx) => rIdx === j ? e.target.value : r) } : ch))}
+                fullWidth
+                variant="outlined"
+                margin="normal"
+              />
+            ))}
+            <IconButton title="Delete" onClick={() => setChallenges(challenges.filter((_, idx) => i !== idx))}>
+              <Delete />
+            </IconButton>
+          </AccordionDetails>
+        </Accordion>
       ))}
       <button onClick={() => setChallenges([...challenges, { Main: "", Related: ["", "", "", ""] }])} title="Add Challenge">
         Add
