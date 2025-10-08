c575098b2988df9e4cdd7e88b1e53f78e2c11249
tjasz <nukorausa@hotmail.com>
2025-10-07 20:33:46 -0700

    Fix style of Home buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,26 +9,23 @@ function Home() {
     LocalStorageKeys.GAME_STATE
   );
 
-  return <div>
+  return <div id="home">
     <Button variant="contained" color="primary" onClick={() => {
       removeGameState();
       navigate("/play");
     }}>
       New Game
     </Button>
-    <br />
     <Button variant="contained" color="primary" disabled={!gameState} onClick={() => {
       navigate("/play");
     }}>
       Resume Game
     </Button>
-    <br />
     <Button variant="contained" color="primary" onClick={() => {
       navigate("/guide");
     }}>
       How to Play
     </Button>
-    <br />
     <Button variant="contained" color="primary" onClick={() => {
       navigate("/settings");
     }}>
